207de32991ac67c64241e57bbdd2d435002e175f
Mahmoud Ebrahim <142084714+bandito19@users.noreply.github.com>
2024-10-13 13:50:54 +0300

    Update poly-fitting.py

--- polynomial-fitting/poly-fitting.py
+++ polynomial-fitting/poly-fitting.py
@@ -1,17 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.array([1, 1.5, 2, 2.5, 3])
-y = np.array([1.41, 1.96, 2.88, 4.16, 5.8])
-N = 2
+# Degree of polynomial
+N = int(input("What is the degree of the polynomial: "))
 
+# Data points of experiment
+x_points = input("Input X_values seperated by a space: ")
+y_points = input("Input Y_values seperated by a space: ")
+
+xs = x_points.split()
+ys = y_points.split()
+
+x = np.array([int(i) for i in xs])
+y = np.array([int(i) for i in ys])
+
+# Vandermonde Matrix of x data points
 A = np.vander(x, N + 1)
-# print(A)
 
-coff, resid, rank, s = np.linalg.lstsq(A, y)
+# Unkown coefficients 
+coeff, resid, rank, s = np.linalg.lstsq(A, y)
 
-polynomial = np.poly1d(coff)
+# polynomial form
+polynomial = np.poly1d(coeff)
 
+# plotting the results
 plt.scatter(x,y, color='green', label='Date points')
 x_line = np.linspace(min(x), max(x), 100)
 plt.plot(x_line, polynomial(x_line), color='red', label=f'y = {polynomial}')
